2bd769cc0482bcbd9d3f95dfa1c9fe25e6a05f3c
BitH0xer <be.coder.19@gmail.com>
2024-01-19 22:52:15 +0530

    Time: 68 ms (60.00%) | Memory: 53.1 MB (5.71%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.ts
@@ -0,0 +1,22 @@
+/* TypeScript */
+
+function minFallingPathSum(matrix: number[][]): number {
+    const M: number = matrix.length;
+    const N: number = matrix[0].length;
+
+    let prevRow: number[] = [...matrix[0]]; // Initialize with the first row
+
+    for (let r: number = 1; r < M; r++) {
+        const currRow: number[] = new Array(N).fill(0);
+        for (let c: number = 0; c < N; c++) {
+            const curr: number = matrix[r][c];
+            const top: number = curr + prevRow[c];
+            const topL: number = curr + (prevRow[c - 1] || Infinity);
+            const topR: number = curr + (prevRow[c + 1] || Infinity);
+            currRow[c] = Math.min(top, topL, topR);
+        }
+        prevRow = [...currRow]; // Update the previous row with the current row
+    }
+
+    return Math.min(...prevRow);
+}
