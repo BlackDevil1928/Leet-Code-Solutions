63bce09d60308018dd0820df1bc2a34fd2bae86f
BitH0xer <be.coder.19@gmail.com>
2024-01-25 12:12:18 +0530

    Time: 33 ms (25.36%) | Memory: 15.3 MB (59.64%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.go
+++ 1250-longest-common-subsequence/longest-common-subsequence.go
@@ -1,28 +1,41 @@
 func longestCommonSubsequence(text1 string, text2 string) int {
-    if len(text1) > len(text2){
-        text1,text2 = text2,text1
+    dp := make([][]int, len(text1)+1)
+    
+    for i := 0; i < len(text1)+1; i++ {
+        dp[i] = make([]int, len(text2)+1)
     }
-    prev := make([]int, len(text1)+1)
-    curr := make([]int, len(text1)+1)
-
-    for i:=1;i<len(text2)+1;i++{
-        letter1 := text2[i-1]
-        for j:=1;j<len(text1)+1;j++{
-            letter2 := text1[j-1]
-            if letter1 == letter2{
-                curr[j] = prev[j-1]+1
-            }else{
-                curr[j] = max(prev[j],curr[j-1])
-            } 
+    
+    for i := 0; i < len(text1)+1; i++ {
+        for j := 0; j < len(text2)+1; j++ {
+            dp[i][j] = -1
         }
-        curr,prev = prev,curr
     }
-    return prev[len(text1)]
+    
+    return lcs(0, 0, text1, text2, &dp)
 }
 
-func max(a,b int)int{
-    if a>b{
-        return a
+func lcs(i int, j int, str1 string, str2 string, dp *[][]int) int {
+    if (*dp)[i][j] != -1 {
+        return (*dp)[i][j]
     }
+    
+    var cache int
+    
+    if i == len(str1) || j == len(str2) {
+        cache = 0
+    } else if str1[i] == str2[j] {
+        cache = lcs(i+1, j+1, str1, str2, dp) + 1
+    } else {
+        firstCache := lcs(i+1, j, str1, str2, dp)
+        secondCache := lcs(i, j+1, str1, str2, dp)
+        cache = max(firstCache, secondCache)
+    }
+    
+    (*dp)[i][j] = cache
+    return cache
+}
+
+func max(a, b int) int {
+    if a > b { return a }
     return b
 }
