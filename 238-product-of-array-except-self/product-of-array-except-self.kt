f63cd0a2adfdef34fed73cc988377ae66b1d0b27
BitH0xer <be.coder.19@gmail.com>
2024-03-15 21:37:11 +0530

    Time: 298 ms (70.34%) | Memory: 49.8 MB (44.71%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.kt
@@ -0,0 +1,22 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val n = nums.size
+        val left = IntArray(n) { 1 }
+        val right = IntArray(n) { 1 }
+
+        for (i in 1 until n) {
+            left[i] = left[i - 1] * nums[i - 1]
+        }
+
+        for (i in n - 2 downTo 0) {
+            right[i] = right[i + 1] * nums[i + 1]
+        }
+
+        val result = IntArray(n) { 0 }
+        for (i in 0 until n) {
+            result[i] = left[i] * right[i]
+        }
+
+        return result
+    }
+}
