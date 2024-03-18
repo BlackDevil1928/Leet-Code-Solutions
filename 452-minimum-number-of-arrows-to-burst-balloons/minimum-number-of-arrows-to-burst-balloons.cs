1c9ebc113bfd542c81f7064607ef2ab4949797fa
BitH0xer <be.coder.19@gmail.com>
2024-03-18 19:48:24 +0530

    Time: 579 ms (70.25%) | Memory: 80.9 MB (64.31%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int FindMinArrowShots(int[][] points) {
+        // Sort the balloons based on their end coordinates
+        Array.Sort(points, (a, b) => a[1].CompareTo(b[1]));
+
+        int arrows = 1;
+        int prevEnd = points[0][1];
+
+        // Count the number of non-overlapping intervals
+        for (int i = 1; i < points.Length; ++i) {
+            if (points[i][0] > prevEnd) {
+                arrows++;
+                prevEnd = points[i][1];
+            }
+        }
+
+        return arrows;
+    }
+}
