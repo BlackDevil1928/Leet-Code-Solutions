7930b51a245a3916dc4e9e8ab19a7e67e2a540df
BitH0xer <be.coder.19@gmail.com>
2024-03-26 21:06:15 +0530

    Time: 64 ms (19.30%) | Memory: 44.8 MB (26.01%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
+    bool search(int k,vector<int>&v) {
+        int i=0,j=v.size()-1;
+        while(i<=j) {
+            int m=(i+j)/2;
+            if(k==v[m])
+                return false;
+            if(k>v[m])
+                i=m+1;
+            else
+                j=m-1;
+        }
+        return true;
+    }
     int firstMissingPositive(vector<int>& v) {
         int n=v.size();
-        set<int> s;
-        for(auto i:v)
-            s.insert(i);
+        sort(v.begin(),v.end());
         for(int i=1;i<=100001;i++) {
-            if(s.find(i)==s.end())
+            if(search(i,v))
                 return i;
         }
         return 0;
