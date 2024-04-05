5d8e45f0bafbef9211d2bd268d68701307c0a725
BitH0xer <be.coder.19@gmail.com>
2024-04-05 17:07:46 +0530

    Time: 63 ms (11.17%) | Memory: 48.9 MB (50.72%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,14 @@
+function maxDepth(s) {
+    let count = 0;
+    let max = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            count++;
+        }
+        max = Math.max(count, max);
+        if (s[i] === ')') {
+            count--;
+        }
+    }
+    return max;
+}
