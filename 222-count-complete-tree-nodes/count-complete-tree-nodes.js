fa0ef4988d8ea63bd3523886fe9b079a8c9299c1
BitH0xer <be.coder.19@gmail.com>
2024-05-03 11:01:42 +0530

    Time: 68 ms (89.92%) | Memory: 67.8 MB (72.40%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function (root) {
+	if (!root) {
+		return 0;
+	}
+
+	const countRight = countNodesByLevel(root, 'right');
+	const countLeft = countNodesByLevel(root, 'left');
+
+	// Count
+	if (countRight === countLeft) {
+		return countRight;
+	}
+
+	/**
+	 * right nodes + left nodes + the root
+	 */
+	return countNodes(root.right) + countNodes(root.left) + 1;
+};
+
+/**
+ * Count the nodes by level by traversing in the passed direction only.
+ * @param {TreeNode | null} root
+ * @param {'right' | 'left'} dir
+ */
+function countNodesByLevel(root, dir) {
+	let levels = 0;
+	let node = root;
+
+	while (node) {
+		node = node[dir];
+		levels++;
+	}
+
+	return Math.pow(2, levels) - 1;
+}
