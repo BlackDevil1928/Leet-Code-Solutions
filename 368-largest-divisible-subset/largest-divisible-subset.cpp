af2a50da96b63910c81331db1175bd2371b92807
BitH0xer <be.coder.19@gmail.com>
2025-04-06 18:23:37 +0530

    Time: 13 ms (39.14%) | Memory: 12.4 MB (91.73%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,31 +1,22 @@
-
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        int n=nums.size(), maxi=1, num=-1;
-        vector<int>v;
         sort(nums.begin(), nums.end());
-        vector<int>dp(n, 1);
-        for(int i=1; i<n; i++){
-            for(int j=0; j<i; j++){
-                if(!(nums[i]%nums[j]) && dp[i]<dp[j]+1){
-                    dp[i]=dp[j]+1;
-                    if(maxi<dp[i]){
-                        maxi=dp[i];
-                    }
+        vector<int> dp(nums.size(), 1), prev(nums.size(), -1);
+        int maxi = 0;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
                 }
             }
+            if (dp[i] > dp[maxi]) maxi = i;
         }
-        for(int i=n-1; i>=0; i--){
-            if(maxi==dp[i] && (num==-1 || !(num%nums[i]))){
-                v.push_back(nums[i]);
-                maxi--;
-                num=nums[i];
-            }
+        vector<int> res;
+        for (int i = maxi; i >= 0; i = prev[i]) {
+            res.push_back(nums[i]);
         }
-        return v;
+        return res;
     }
 };
-
-
-
