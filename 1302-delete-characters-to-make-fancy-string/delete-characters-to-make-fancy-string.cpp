2caa993b4404ceafd78361774a23c0e5a9b89200
BitH0xer <be.coder.19@gmail.com>
2025-07-21 15:18:30 +0530

    Time: 28 ms (56.09%) | Memory: 44.4 MB (85.03%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -1,19 +1,17 @@
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     string makeFancyString(string s) {
-        string ans = "";
-        ans.push_back(s[0]);
-        int n = s.size(),cnt = 1;
-        for(int i=1;i<n;i++){
-            if(s[i] == ans.back()){
-                cnt++;
-                if(cnt < 3)ans.push_back(s[i]);
+        string result;
+        for (char c : s) {
+            if (result.size() >= 2 && result.back() == c && result[result.size() - 2] == c) {
+                continue;
             }
-            else{
-                cnt = 1;
-                ans.push_back(s[i]);
-            }
+            result.push_back(c);
         }
-        return ans;
+        return result;
     }
 };
