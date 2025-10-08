c4f8ed409bf6bee59335bbaeb6d26ba60f5e9659
silkysharma15 <bsidesjammu.pralinkh@gmail.com>
2025-10-08 21:40:51 +0530

    Time: 470 ms (60.04%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,5 +1,6 @@
+SELECT today.id
+FROM Weather yesterday 
+CROSS JOIN Weather today
 
-select distinct id
-from Weather
-where prior temperature < temperature
-connect by prior recordDate +1 = recordDate
+WHERE DATEDIFF(today.recordDate,yesterday.recordDate) = 1
+    AND today.temperature > yesterday.temperature
